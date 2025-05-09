693cbe2474cc4deabd7a011c6baeeefaa7d319ad
parthpbhagat <parthpbhagat@gmail.com>
2025-05-09 16:57:02 +0530

    Add files via upload

---
+++ parth/day10.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+
+    // int num1, num2;
+    // printf("enter the value of a frist number", num1);
+    // scanf("%d/n", &num1);
+    // printf("enter a value of a secand number", num2);
+    // scanf("%d/n", &num2);
+    // if (num1<num2)
+    // {
+    //     printf("the minimum value is%d",num1);
+    // }else
+    // {
+    //     printf("the minimum value is%d",num2);
+    // }
+
+    // int num1;
+    // printf("enter any number:", num1);
+    // scanf("%d", &num1);
+    // if (num1 < 0)
+    // {
+    //     printf("this number is negitive");
+    // }
+    // else if (num1 > 0)
+    // {
+    //     printf("this number is positive");
+    // }
+    // else
+    // {
+    //     printf("this number is nutrial");
+    // }
+  
+  
+
+
+    return 0;
+}
