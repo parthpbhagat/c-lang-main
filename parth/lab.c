1da82b365b7ef7728c720ca0c29e0d88a9ffd8e5
parthpbhagat <parthpbhagat@gmail.com>
2025-05-14 14:14:29 +0530

    Add files via upload

--- parth/lab.c
+++ parth/lab.c
@@ -27,32 +27,36 @@ int main()
 
     // int num;
     // printf("enter any number:");
-    // scanf("%d",&num);
-    // while (num>=1)
-    // {if (num % 2==1)
+    // scanf("%d", &num);
+    // while (num >= 1)
     // {
-    //     printf("%d \t",num);
-
-    // }num--;
+    //     if (num % 2 == 1)
+    //     {
+    //         printf("%d \t", num);
+    //     }
+    //     num--;
     // }
-    int start, end;
-    printf("enter the frist number: ");
-    scanf("%d", &start);
 
-    printf("enter the secand number: ");
-    scanf("%d", &end);
 
-    printf("Leap years between %d and %d are:\t", start, end);
+// int num=10;
+// do
+// {
+//     printf("%d\n",num);
+//     num--;
+// } while (num>=1);
 
-    while (start <= end)
-    {
+// fibonacci series
+int n,a=0,b=1,c,i;
+printf("enter number of turn ");
+scanf("%d",&n);
+for ( i = 0; i < n; i++)
+{
+    printf("%d\t",a); // 0+1=1      
+    c=a+b; 
+    a=b;
+    b=c; 
+}
 
-        if ((start % 4 == 0 && start % 100 != 0) || (start % 400 == 0))
-        {
-            printf("%d\t", start);
-        }
-        start++;
-    }
 
-    return 0;
+return 0;
 }
