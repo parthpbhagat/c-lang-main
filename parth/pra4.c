ee67918f94357966e36ceb2e49b1627460a8d05e
parthpbhagat <parthpbhagat@gmail.com>
2025-05-20 13:35:26 +0530

    Add files via upload

---
+++ parth/pra4.c
@@ -0,0 +1,111 @@
+// #include<stdio.h>
+// int main(){
+//     for (int i = 41; i <=45; i++)
+//     {for (int j = 41; j <=i; j++)
+//     {
+//         printf("%d ",j);
+//     }
+
+//       printf("\n");
+//     }
+
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main (){
+//    int num=11;
+//    for (int i = 1; i <=4; i++)
+//    {for (int j = 1; j <=i; j++)
+//    {
+//     printf("%d",num);
+//     num++;
+//    }
+
+//     printf("\n");
+//    }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i, j, n = 5;
+//     for (i = 1; i <= 5; i++)
+//     {
+//         for (j = 1; j <= i; j++)
+//         {
+//             printf("%d", j);
+//         }
+//         for (j = 1; j <= 2 * (n - i); j++)
+//         {
+//             printf(" ");
+//         }
+//         for (j = i; j >= 1; j--)
+//         {
+//             printf("%d", j);
+//         }
+
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for (int i = 1; i <= 5; i++)
+//     {for (int j = 1; j <=i; j++)
+//     {
+//         // printf("%d",j);
+//         printf("*",j);
+//     }
+
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         if (i == 1 || i == 3)
+//         {
+//             printf("*****");
+//         }
+//         else if (i == 2)
+//         {
+//             printf("*   *");
+//         }
+//         else if (i == 4)
+//         {
+//             printf("*");
+//         }
+//         else
+//         {
+//             printf("*");
+//         }
+
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main(){
+//     for (int i = 1; i <=5; i++)
+//     {for (int j = 5; j >=i; j--)
+//     {
+//         printf("%d",j);
+//     }
+    
+//         printf("\n");
+//     }
+    
+//     return 0;
+// }
