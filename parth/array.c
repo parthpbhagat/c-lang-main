e7ab413aa5e13e64f1df221a49ef105e8db19972
parthpbhagat <parthpbhagat@gmail.com>
2025-05-20 13:43:05 +0530

    Add files via upload

---
+++ parth/array.c
@@ -0,0 +1,32 @@
+// #include <stdio.h>
+
+// int main() {
+//     int size, i;
+
+//     printf("Enter the size of the arrays: ");
+//     scanf("%d", &size);
+
+//     int a[size], b[size], sum[size];
+
+//     printf("Enter elements of first array:\n");
+//     for(i = 0; i < size; i++) {
+//         scanf("%d", &a[i]);
+//     }
+
+//     printf("Enter elements of second array:\n");
+//     for(i = 0; i < size; i++) {
+//         scanf("%d", &b[i]);
+//     }
+
+//     for(i = 0; i < size; i++) {
+//         sum[i] = a[i] + b[i];
+//     }
+
+//     printf("Sum array:\n");
+//     for(i = 0; i < size; i++) {
+//         printf("%d ", sum[i]);
+//     }
+
+//     return 0;
+// }
+
