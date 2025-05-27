466555344fdf4a8b1ecfe58d850586ce3a8d6ef3
parthpbhagat <parthpbhagat@gmail.com>
2025-05-27 13:50:26 +0530

    Add files via upload

--- parth/calculater.c
+++ parth/calculater.c
@@ -1,49 +1,49 @@
-// #include <stdio.h>
-// int main()
-// {
-//     float num1;
-//     float num2;
-//     char opraters; // ASCII
-//     float result;
+#include <stdio.h>
+int main()
+{
+    float num1;
+    float num2;
+    char opraters; // ASCII
+    float result;
 
-//     printf("enter the number:");
-//     scanf("%f", &num1);
-//     printf("enter the opreters:");
-//     scanf(" %c", &opraters);
+    printf("enter the number:");
+    scanf("%f", &num1);
+    printf("enter the opreters:");
+    scanf(" %c", &opraters);
     
-//     // printf("%d", opraters);
+    // printf("%d", opraters);
 
-//     printf("enter the number:", num2);
-//     scanf("%f", &num2);
+    printf("enter the number:", num2);
+    scanf("%f", &num2);
 
-//     switch (opraters)
-//     {
-//     case '+':
-//         result = num1 + num2;
-//         printf("%f", result);
-//         break;
+    switch (opraters)
+    {
+    case '+':
+        result = num1 + num2;
+        printf("%f", result);
+        break;
 
-//     case '-':
-//         result = num1 - num2;
-//         printf("%f", result);
-//         break;
+    case '-':
+        result = num1 - num2;
+        printf("%f", result);
+        break;
 
-//     case '*':
-//         result = num1 * num2;
-//         printf("%f", result);
-//         break;
+    case '*':
+        result = num1 * num2;
+        printf("%f", result);
+        break;
 
-//     case '/':
-//         result = num1 / num2;
-//         printf("%f", result);
-//         break;
+    case '/':
+        result = num1 / num2;
+        printf("%f", result);
+        break;
 
-//     default:
-//         printf("invalid opreters");
+    default:
+        printf("invalid opreters");
 
-//         break;
-//     }
+        break;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
