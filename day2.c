bf5b0675c0e18a462e53a795f2f6067583cf6bd4
parthpbhagat <parthpbhagat@gmail.com>
2025-06-10 14:52:57 +0530

    Add files via upload

---
+++ day2.c
@@ -0,0 +1,132 @@
+// Q1 table use for loop and while loop
+
+// #include<stdio.h>
+// int main (){
+//     int num;
+//     printf("enter a number to display its multiplication table:");
+//     scanf("%d",&num);
+//     printf("multiplication table of: %d\n",num);
+//     for(int i=1;i<=10;i++)
+//     {
+//         printf("%d*%d=%d\n",num,i,num*i);
+//     };
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main (){
+//     int num,i=1;
+//     printf("enter a number to display its multiplication table:");
+//     scanf("%d",&num);
+//     printf("multiplication table of: %d\n",num);
+//     while(i<=10)
+//     {
+//         printf("%d*%d=%d\n",num,i,num*i);
+//         i++;
+//     };
+//     return 0;
+// }
+
+// Q2 sum of frist 10 natural number use for loop and while number
+
+// #include<stdio.h>
+// int main (){
+//     int i,sum=0;
+//     for(i=1;i<=10;i++)
+//     {
+//         sum+=i;
+//     };
+//     printf("sum of frist 10 number is:%d",sum);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main (){
+//     int i=1,sum=0;
+//   while(i<=10)
+//     {
+//         sum+=i;
+//         i++;
+//     };
+//     printf("sum of frist 10 number is:%d",sum);
+//     return 0;
+// }
+
+// Q3 display of cube use for loop and while loop
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i, num;
+
+//     printf("Enter the number of terms: ");
+//     scanf("%d", &num);
+//     for (i = 1; i <= num; i++)
+//     {
+//         printf("cube is: %d\n", i * i * i);
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i = 1, num;
+
+//     printf("Enter the number of terms: ");
+//     scanf("%d", &num);
+
+//     while (i <= num)
+//     {
+//         printf("%d\n", i * i * i);
+//         i++;
+//     }
+
+//     return 0;
+// }
+
+// Q4 display the natural number using for loop and while loop
+
+// #include<stdio.h>
+// int main (){
+//     int i=1;
+//     for (int i = 1; i <=10; i++)
+//     {
+//         printf("%d\n",i);
+//     };
+
+//     while (i<=10)
+//     {
+//         printf("%d\n",i);
+//         i++;
+//     }
+    
+    
+//     return 0;
+// }
+
+
+// Q5 display natural number using for loop and whilw loop (10 to 1)
+
+
+// #include<stdio.h>
+// int main(){
+//     int i=10;
+//    for (int i = 10; i >=1; i--)
+//    {
+//     printf("%d\n",i);
+//    };
+
+//    while (i>=1)
+//    {
+//      printf("%d\n",i);
+//      i--;
+//    }
+   
+   
+    
+//     return 0;
+// }
