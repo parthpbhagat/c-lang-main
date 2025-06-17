5dfd1be46dd75eb7c298c9e0f1f124962ce5512b
parthpbhagat <parthpbhagat@gmail.com>
2025-06-17 16:51:48 +0530

    Add files via upload

---
+++ parth/python/parth.py
@@ -0,0 +1,94 @@
+        # import calendar
+
+# year = int(input("enter year:"))
+# month = int(input("enter month:"))
+
+# cal =calendar.month(year,month)
+# print(cal)
+
+       # symple print function
+# print ("hello python")
+
+       # # addition
+# num1 = float(input("enter the frist number for addition:"))
+# num2 = float(input("enter the secound number for addition:"))
+
+# sum_result = num1+num2
+# print(f"sum:{num1}+{num2}={sum_result}")
+
+
+      # # multiplication
+# num1 = int(input("enter frist number:"))
+# num2 = int(input("enter secound number:"))
+
+# sum_result=num1*num2
+# print(f"multiplication:{num1}*{num2}={sum_result}")
+
+
+      # # division
+# num1 = int(input("enter frist number:"))
+# num2 = int(input("enter secound number:"))
+
+# sum_result=num1/num2
+# print(f"division:{num1}/{num2}={sum_result}")
+
+
+       # # subtraction
+# num1 = int(input("enter frist number:"))
+# num2 = int(input("enter secound number:"))
+
+# sum_result=num1-num2
+# print(f"division:{num1}-{num2}={sum_result}")
+
+
+      # random number
+# import random
+# print(f"rendom number:{random.randint(1, 100)}")
+
+
+       # # Odd or even number check
+# num = int(input("Enter any number: "))
+
+# if num % 2 == 0:
+#     print(f"This is an even number.")
+# else:
+#     print(f"This is an odd number.")
+
+
+        # # swapping
+# a=10
+# b=15
+
+# # swapping
+# a,b = b,a
+# print ("after swaping")
+# print ("a=",a)
+# print ("b=",b)
+
+
+         # # multiplation table
+# num = int(input("multiplation table"))
+# for i in range(1,11):
+#     print(f"{num}*{i}={num*i}")
+
+        # patterns
+# print("*")
+# print("**")
+# print("***")
+# print("**")
+# print("*")
+
+            # display a formatted massage
+# name = (input("name" ))
+# age = (input("age" ))
+# hobby = (input("hobby" ))
+# print("Hello,"+ name +"!")
+# print("At "+ age + ",enjoying")
+# print(hobby + " sounds fun!")
+
+
+
+
+
+
+
---
+++ parth/python/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+# odd number
+# num = int(input("enter any number:"))
+
+# if num % 2 == 0:
+# print("this is a even number:")
+
+# else:
+# print("this is a odd number:")
