c904f260c29803427b94a01d3dd8d8fd5e03af19
parthpbhagat <parthpbhagat@gmail.com>
2025-06-25 16:13:07 +0530

    Add files via upload

--- python/parth.py
+++ python/parth.py
@@ -1,119 +1,243 @@
+# import calendar
+
+# year = int(input("enter year:"))
+# month = int(input("enter month:"))
+
+# cal = calendar.month(year, month)
+# print(cal)
 
 
-# list = [1,2,3,4,5,6,7]
-# list.insert(7,8)
+# # symple print function
+# print("hello python")
+
+
+# # # addition
+# num1 = float(input("enter the frist number for addition:"))
+# num2 = float(input("enter the secound number for addition:"))
+
+# sum_result = num1 + num2
+# print(f"sum:{num1}+{num2}={sum_result}")
+
+
+# # multiplication
+# num1 = int(input("enter frist number:"))
+# num2 = int(input("enter secound number:"))
+
+# sum_result = num1 * num2
+# print(f"multiplication:{num1}*{num2}={sum_result}")
+
+
+# # division
+# num1 = int(input("enter frist number:"))
+# num2 = int(input("enter secound number:"))
+
+# sum_result = num1 / num2
+# print(f"division:{num1}/{num2}={sum_result}")
+
+
+# # subtraction
+# num1 = int(input("enter frist number:"))
+# num2 = int(input("enter secound number:"))
+
+# sum_result = num1 - num2
+# print(f"division:{num1}-{num2}={sum_result}")
+
+
+# #   random number
+# import random
+
+# print(f"rendom number:{random.randint(1, 100)}")
+
+
+# # Odd or even number check
+# num = int(input("Enter any number: "))
+
+# if num % 2 == 0:
+#     print(f"This is an even number.")
+# else:
+#     print(f"This is an odd number.")
+
+#     # swapping
+# a = 10
+# b = 15
+
+# # swapping
+# a, b = b, a
+# print("after swaping")
+# print("a=", a)
+# print("b=", b)
+
+
+# # multiplation table
+# num = int(input("multiplation table"))
+# for i in range(1, 11):
+#     print(f"{num}*{i}={num*i}")
+
+#     # patterns
+# print("*")
+# print("**")
+# print("***")
+# print("**")
+# print("*")
+
+# # display a formatted massage
+# # name = (input("name" ))
+# # age = (input("age" ))
+# # hobby = (input("hobby" ))
+# # print(f"Hello," ,name + "!" "At ",age  ",enjoying" hobby  " sounds fun!")
+
+
+# for i in range(1, 6):  # Outer loop (rows)
+#     for j in range(i):  # Inner loop (columns)
+#         print("*", end=" ")
+#     print()
+
+
+# num = int(input("any num:"))
+# print("multiplacition table of")
+# for i in range(1, 11):
+#     print(f"{num}*{i}={num*i}")
+
+
+# for i in range(1, 6):
+#     for j in range(1, i):
+#         print("*", end=" ")
+#     print()
+
+
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
+# for i in range(4, 0, -1):
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
+
+
+# for i in range(1, 6):
+#     for j in range(1, 7 - i):
+#         print(j, end=" ")
+#     print()
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
+
+
+# for i in range(1, 6):
+#     for k in range(1, i):
+#         print(" ", end=" ")
+#     for j in range(i, 6):
+#         print(j, end=" ")
+#     print()
+# for i in range(1, 6):
+#     for k in range(1, 7 - i):
+#         print("", end=" ")
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
+
+
+# # 1234554321
+# # 1234  4321
+# # 123    321
+# # 12      21
+# # 1        1
+# # 1        1
+# # 12      21
+# # 123    321
+# # 1234  4321
+# # 1234554321
+
+
+# def print_pattern():
+#     n = 5
+
+#     # Top half
+#     for i in range(n):
+
+#         for j in range(1, n - i + 1):
+#             print(j, end="")
+
+#         print(" " * (2 * i), end="")
+
+#         for j in range(n - i, 0, -1):
+#             print(j, end="")
+
+#         print()
+
+#     # Bottom half
+#     for i in range(n):
+
+#         for j in range(1, i + 2):
+#             print(j, end="")
+
+#         print(" " * (2 * (n - i - 1)), end="")
+
+#         for j in range(i + 1, 0, -1):
+#             print(j, end="")
+
+#         print()
+
+
+# print_pattern()
+
+# sDict = {x.upper(): x * 3 for x in "coding"}
+# print(sDict)
+# sDict = {x.lower(): x * 4 for x in "PARTH"}
+# print(sDict)
+
+
+# # Nested list comprehension
+# matrix = [[j for j in range(5)] for i in range(3)]
+
+# print(matrix)
+# print(matrix)
+
+# parth = [[j for j in range(1, 5)] for i in range(4)]
+
+# print(parth)
+# print(parth)
+
+
+# list = [charector for charector in "parth"]
+# print(list)
+
+# list = []
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         list.append(j)
 # print(list)
 
 
-
-# information = {
-#     "name":"parth bhagat",
-#     "age":27,
-#     "mo no":9011494385,
-#     "address":"ahemdabad"
-# }
-# print(information)
+# list = [i for i in range(20) if i % 2 == 1]
+# print(list)
 
 
-# frist_name = input("enter your name:")
-# last_name = input("enter your last name:")
-# print("hello",last_name,frist_name)
+# list = [i for i in range(2000, 3000) if i % 4 == 0]
+# print(list)
+
+# list = [i for i in range(4, 41) if i % 4 == 0]
+# print(list)
 
 
-                #   cafe orderd menu
-# menu = {
-#     "pizza":100,
-#     "sandwich":50,
-#     "pasta":60,
-#     "coffi":40,
-#     "cold coffi":100
-# }
-# print("----------welcome to our cafe------------")
-# print(" 1.pizza:         100\n 2.sandwich:         50\n 3.pasta:      60\n 4.coffi:       40\n 5.cold coffi:        100")
-
-# order_total = 0
-# item1 = input("enter name of order")
-# if item1 in menu:
-#     order_total+= menu[item1]
-#     print(f"your iteam {item1} has been added")
-
-# else:
-#     print(f"your iteam {item1} not avalable")
-# anather_order = input("do you want to add anather order? (yes/no)")
-# if anather_order == "yes":
-#         item2 = input("enter the name of secound iteam =")
-# if item2 in menu:
-#  order_total+=menu [item2]
-#  print(f"iteam2 {item2} has been added")
-# else:
-#  print(f"your item {item2} not avalable")
-# print(f"plase total amount to you pay {order_total} rs ")
+# for i in range(1, 11):
+#     print(i)
+# for i in range(10, 0, -1):
+#     print(i)
 
 
 
-# print my name 10 time
-
-# name = input("enter your name :")
-# x = 1
-# while x <=10:
-#     print(x)
-#     x = x + 1
-
-
-
-#       multiplaction table using loop
-# num =int(input("enter the num:"))
-# x = 1
-# while x <=10:
-#     print(f"{num}*{x}={num*x}")
-#     x = x + 1
-
-
-
-
-    #   nested for loop 
-# x = 1
-# for x in range (1,5):
-#  for j in range (1,5):
-#     print("*",end=" ")
-#  print()
-
-
-
-#  print this pattern
-"""
-*
-**
-***
-****
-*****
-"""
-# r = 5
-# for x in range (r,0,-1):
-#  for j in range (x):
-#     print("*",end=" ")
-#  print()
-
-
-
-
-#  r = 5
-# for x in range (1,r+1):
-#  for j in range (1,x+1):
-#     print("*",end=" ")
-#  print()
-
-
-
-r = 6
-for x in range(1,r):
-   for j in range(1, x + 1):
-      print(j ,end=" ")
-   print()
-
-    
-
-
-
-
+percentage = int(input("enter your score: "))
+if percentage >= 90:
+    print("excellent work! ")
+elif percentage >= 80:
+    print("well done")
+elif percentage >= 70:
+    print("good job")
+elif percentage >= 60:
+    print("you pass")
+else:
+    print("sorry you fail")
 
